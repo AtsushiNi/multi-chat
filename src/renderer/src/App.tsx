d0029afff0243c8ec532934f2ab1f131447368d1
AtsushiNi <atsushi9731@gmail.com>
2025-03-22 11:44:42 +0900

    commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,12 @@
+import Antdx from './components/Antdx';
+import './assets/main.css';
+
+function App(): JSX.Element {
+  return (
+    <div style={{ height: '100vh' }}>
+      <Antdx />
+    </div>
+  );
+}
+
+export default App;
